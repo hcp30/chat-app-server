5c8bab560b72ddcdc8febf0d3eeb19c196a00194
Hiren Patel <hirenp231994@gmail.com>
2025-02-26 23:57:26 -0500

    server accepts client message and returns client message

--- src/ServerApplication.java
+++ src/ServerApplication.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,9 +9,16 @@ public class ServerApplication {
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
             Socket socket = serverSocket.accept();
             System.out.println("Connection Accepted!");
+            OutputStream os = socket.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(os,true);
+//            os.write("Connected to server.".getBytes());
             InputStream is = socket.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            System.out.println(br.readLine());
+            String messageFromClient;
+            while ((messageFromClient = br.readLine()) != null) {
+                System.out.println("From Client: " + messageFromClient);
+                printWriter.println(messageFromClient);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
