b69d6fec454fc60f45edee8cd7248350580cbb10
Hiren Patel <hirenp231994@gmail.com>
2025-02-28 00:57:13 -0500

    server can accept multiple clients

--- src/ServerApplication.java
+++ src/ServerApplication.java
@@ -7,17 +7,33 @@ public class ServerApplication {
         System.out.println("server!");
 
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Connection Accepted!");
-            OutputStream os = socket.getOutputStream();
-            PrintWriter printWriter = new PrintWriter(os,true);
-//            os.write("Connected to server.".getBytes());
-            InputStream is = socket.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String messageFromClient;
-            while ((messageFromClient = br.readLine()) != null) {
-                System.out.println("From Client: " + messageFromClient);
-                printWriter.println(messageFromClient);
+            Thread main = Thread.currentThread();
+            System.out.println("Thread: " + main.getName());
+            Socket socket;
+            while ((socket = serverSocket.accept()) != null){
+                Socket finalSocket = socket;
+                Thread thread = new Thread(() -> {
+                    try {
+                        System.out.println("Connection Accepted!");
+                        Thread currThread = Thread.currentThread();
+                        System.out.println("Thread: " + currThread.getName());
+                        OutputStream os = finalSocket.getOutputStream();
+                        PrintWriter printWriter = new PrintWriter(os, true);
+                        InputStream is = finalSocket.getInputStream();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                        String messageFromClient;
+                        while ((messageFromClient = br.readLine()) != null) {
+                            System.out.println("From Client: " + messageFromClient);
+                            printWriter.println(messageFromClient);
+                        }
+                        finalSocket.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+                thread.start();
+                System.out.println("IT HAS ENDED!!!!!!!!!!");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
