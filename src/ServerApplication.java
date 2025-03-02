f75792e2c06c8a48033bc8fbe08fb040682eaeb9
Hiren Patel <hirenp231994@gmail.com>
2025-03-01 19:46:51 -0500

    server can take in user request and forward it to all the other users

--- src/ServerApplication.java
+++ src/ServerApplication.java
@@ -1,14 +1,22 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ServerApplication {
+
+    private final static CopyOnWriteArrayList<String> messageStore = new CopyOnWriteArrayList<>();
+    private final static List<PrintWriter> clientStore = Collections.synchronizedList(new ArrayList<>());
+
     public static void main(String[] args) {
         System.out.println("server!");
 
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
-            Thread main = Thread.currentThread();
-            System.out.println("Thread: " + main.getName());
+            Thread mainThread = Thread.currentThread();
+            System.out.println("Thread: " + mainThread.getName());
             Socket socket;
             while ((socket = serverSocket.accept()) != null){
                 Socket finalSocket = socket;
@@ -19,12 +27,19 @@ public class ServerApplication {
                         System.out.println("Thread: " + currThread.getName());
                         OutputStream os = finalSocket.getOutputStream();
                         PrintWriter printWriter = new PrintWriter(os, true);
+                        clientStore.add(printWriter);
+                        printWriter.println("Connected to server!");
                         InputStream is = finalSocket.getInputStream();
                         BufferedReader br = new BufferedReader(new InputStreamReader(is));
                         String messageFromClient;
                         while ((messageFromClient = br.readLine()) != null) {
-                            System.out.println("From Client: " + messageFromClient);
-                            printWriter.println(messageFromClient);
+                            messageStore.add("user-" + currThread.getName()  + ": "+ messageFromClient);
+                            System.out.println("From Client " + currThread.getName()  + ": "+ messageFromClient);
+                            for (PrintWriter writer: clientStore) {
+                                if (printWriter != writer) {
+                                    writer.println(messageStore.get(messageStore.size() - 1));
+                                }
+                            }
                         }
                         finalSocket.close();
                     } catch (IOException e) {
@@ -33,7 +48,6 @@ public class ServerApplication {
                 });
 
                 thread.start();
-                System.out.println("IT HAS ENDED!!!!!!!!!!");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
